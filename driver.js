2ea7ecc969b02a04597b33a18df9daa1e0bc1262
nathaniel k smith <nathanielksmith@gmail.com>
2013-04-03 13:04:12 -0400

    use settings

--- driver.js
+++ driver.js
@@ -3,40 +3,28 @@
 var path = require('path'),
 request = require('request');
 
-var daemon = require('./daemon'),
-client = require('./client');
-
+var client = require('./client'),
+daemon = require('./daemon'),
+settings = require('./settings');
 require('./util').extend(global, require('./util'));
-
-var HOME = process.env.HOME,
-MUNDANEUMPATH = path.join(HOME, '.mundaneum'),
-STOREPATH = path.join(MUNDANEUMPATH, 'doctore'),
-SSLPATH = path.join(MUNDANEUMPATH, 'ssl'),
-KEYPATH = path.join(SSLPATH, 'mundaneum.key'),
-CERTPATH = path.join(SSLPATH, 'mundaneum.cert'),
-OPENSSLBIN = '/usr/bin/openssl',
-PASSPHRASE = 'secret',
-PIDPATH = path.join(MUNDANEUMPATH, 'server.pid'),
-PORT = 4073,
-HOST = 'localhost';
 
 (function main(argv) {
     if (argv.length <= 2) {return error('usage: TODO');}
 
-    if (argv[2].match(/-s|--start/))
+    if (argv[2].match(/-s|--start/)) {
         return daemon.ensureServer(
-            PASSPHRASE,
-            PORT,
-            HOST,
-            MUNDANEUMPATH,
-            STOREPATH,
-            OPENSSLBIN,
-            SSLPATH,
-            KEYPATH,
-            CERTPATH,
-            PIDPATH,
+            settings.PASSPHRASE,
+            Number(argv[3]) || settings.PORT,
+            settings.HOST,
+            settings.MUNDANEUMPATH,
+            settings.STOREPATH,
+            settings.OPENSSLBIN,
+            settings.SSLPATH,
+            settings.KEYPATH,
+            settings.CERTPATH,
             function(err) { if (err) return error(err) }
         );
+    }
 
     var content = argv.slice(2).join(' ');
     client.postNote(PORT, HOST, PASSPHRASE, content);
